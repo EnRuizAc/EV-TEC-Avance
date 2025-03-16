8ce2865f2859ae125b31c077b752bbb83a71e28f
Enrique Ruiz <jera_24@outlook.com>
2025-03-15 18:00:26 -0600

    ADD Components
    ADD: Se añaden componentes login, skills, listado y home
    ADD: Se añaden header y footer
    ADD: Se añade enrutamiento y botones de navegación en header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ListadoComponent } from './listado/listado.component';
+import { SkillsComponent } from './skills/skills.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'listado', component: ListadoComponent },
+  { path: 'skills', component: SkillsComponent },
+];
